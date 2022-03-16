5f4f09178178d645499255557ec0fba9c25bcf2b
jmwania <josphatmwania237@gmail.com>
2022-03-16 11:38:49 +0300

    Add render function

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ document.addEventListener("click", function(event) {
     // button clikcked
     if (!event.target.matches("#getJoke")) return;
 
-
+// data from the API
     fetch("https://api.chucknorris.io/jokes/random")
     .then((response) => response.json())
     .then((data) => console.log(data));
 
 }); 
+
+function renderJoke(data) {
+    const setup = document.getElementById("setup");
+    const punchline = document.getElementById("punchline");
+    setup.innerHTML = data.setup;
+    punchline.innerHTML = data.punchline;
+
+}
